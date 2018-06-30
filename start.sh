9340db7d01ae7a112d35ac058dd3531c9bdc2f28
Ivan Koveshnikov <ikoveshnik@gmail.com>
2018-07-01 02:22:08 +0500

    choose RSA/ECDSA keys for testing

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-scp nginx.conf tempesta_fw.conf tfw-root.crt tfw-root.key %DUT_SSH%:/tmp
+scp nginx.conf tempesta_fw.conf %KEY%/tfw-root.crt %KEY%/tfw-root.key %DUT_SSH%:/tmp
 scp 0 %DUT_SSH%:/var/www/html/
 ssh %DUT_SSH% 'nginx -c /tmp/nginx.conf'
