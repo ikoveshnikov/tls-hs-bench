13f826aef3ad957e18d80b97a0b1fc3f08d8541b
Ivan Koveshnikov <ikoveshnik@gmail.com>
2018-07-07 17:05:22 +0500

    count dos stats

--- dos_stats.sh
+++ dos_stats.sh
@@ -6,3 +6,7 @@ for i in `seq 1 ${procs}`;
 do
 	tail -n 2 dos_log_${i} | head -n 1 >> dos_log_all
 done
+
+res=`perl -ne '/\[(\d+)/ && print "$1 + ";' dos_log_all`
+echo "Total handshakes per second: `perl -e "print $res 0;"`"
+
